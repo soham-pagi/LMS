5cac24fa1cb6fdaec78e41f663ba59870d00e35c
Soham Pagi <72686728+soham-pagi@users.noreply.github.com>
2022-02-22 23:21:14 +0530

    Create footer.php

---
+++ temp/footer.php
@@ -0,0 +1,9 @@
+<!-- FOOTER -->
+<footer class="bg-dark text-white fixed-bottom" style="margin-bottom: -18px">
+    <div class="container">
+        <div class="row">
+            <p class="text-center small">&copy; 2022 Copyright - All Rights Reserved</p>
+        </div>
+    </div>
+</footer>
+<!-- FOOTER END -->
